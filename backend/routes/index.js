30dc671def0afc9a0be5e41509dbfb50404d82bb
qcd <qn25397@gmail.com>
2025-12-01 20:57:44 +0700

    feat: add brand routes for admin with CRUD operations

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -4,9 +4,11 @@ const authRoutes = require('./api/auth')
 const userRoutes = require('./api/customer/user')
 const categoryController = require('./api/admin/category')
 const { protectCustomer, protectAdmin } = require('../middlewares/user')
+const brandRoutes = require('./api/admin/brand')
 
 router.use('/auth', authRoutes)
 router.use('/users', protectCustomer, userRoutes)
 router.use('/admin/category',protectAdmin,categoryController);
+router.use('/admin/brand',protectAdmin,brandRoutes);
 
 module.exports = router
