9f678696b50fafae87a4f5642bf0125e927e8aaf
qcd <qn25397@gmail.com>
2025-12-01 20:22:20 +0700

    feat: add Brand model

---
+++ backend/models/brandModel.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose')
+
+const brandSchema = mongoose.Schema({
+    name: {
+      type: String,
+      unique: true,
+      required: true,
+      trim: true,
+    },
+    logoUrl: {
+      type: String,
+      default: "",
+    },
+    description: {
+      type: String,
+      default: "",
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Brand = mongoose.model('Brand', brandSchema)
+
+module.exports = Brand;
