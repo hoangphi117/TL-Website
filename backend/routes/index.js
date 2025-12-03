97416aeb465a613599eb7c6f6ef03b430451db19
qcd <qn25397@gmail.com>
2025-12-03 22:38:01 +0700

    feat: rename category and brand route variables for consistency

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -2,18 +2,18 @@ const express = require('express')
 const router = express.Router()
 const authRoutes = require('./api/auth')
 const userRoutes = require('./api/customer/user')
-const categoryController = require('./api/admin/category')
+const categoryRoutesAdmin = require('./api/admin/category')
 const productRoutes = require("./api/product")
 const { protectCustomer, protectAdmin } = require('../middlewares/user')
-const brandRoutes = require('./api/admin/brand')
+const brandRoutesAdmin = require('./api/admin/brand')
 const productRoutesAdmin = require('./api/admin/product')
 
 router.use('/admin/product', protectAdmin, productRoutesAdmin);
 router.use('/auth', authRoutes);
 router.use('/users', protectCustomer, userRoutes)
 
-router.use('/admin/category',protectAdmin,categoryController);
-router.use('/admin/brand',protectAdmin,brandRoutes);
+router.use('/admin/category',protectAdmin,categoryRoutesAdmin);
+router.use('/admin/brand',protectAdmin,brandRoutesAdmin);
 router.use('/products', productRoutes)
 
 
